3e7129760aa1d7135e7b374371b3a2a83580042c
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-08 06:21:25 -0600

    copied some code

--- functions/rd.go
+++ functions/rd.go
@@ -1,45 +1,17 @@
 package lib
 
-//Import the necessary libraries
 import (
 	"bitbucket.org/taubyte/go-sdk/event"
 )
 
-//export
+//export ping
 func rd(e event.Event) uint32 {
-	//Get the database reference
-	// db, err := database.New("testdb")
-	// if err != nil {
-	// 	return 1
-	// }
+  h, err := e.HTTP()
+  if err != nil {
+   return 1
+  }
 
-	// err = db.Put("value/hello", []byte("Hello, world"))
-	// if err != nil {
-	// 	return 1
-	// }
-
-
-	// //Get the data from the database
-	// data,err := db.Get("value/hello")
-	// if err != nil {
-	// 	return 1
-	// }
-
-	//Get HTTP from the event
-	h, err := e.HTTP()
-	if err != nil { //If we get an err 
-		return 1 //roll out
-	}
-
-	//Generate a json response	
-	// jsonData, err := json.MarshalIndent(data, "", "  ")
-	// if err != nil {
-	// 	return 1
-	// }
-
-	//Send the data back to the browser
-	h.Write([]byte("Hello I Work Again"))
-
-	//Return 0 cuz IDK, just do it
-	return 0
+  h.Write([]byte("PONG"))
+  
+  return 0
 }
