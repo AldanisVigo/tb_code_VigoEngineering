3a45ccd1e0676ca71661b1d6bf45b2e66d331f2d
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-08 17:38:12 -0600

    reading from db now

--- functions/readjson.go
+++ functions/readjson.go
@@ -1,17 +1,35 @@
 package lib
 
 import (
+	"encoding/json"
+
+	"bitbucket.org/taubyte/go-sdk/database"
 	"bitbucket.org/taubyte/go-sdk/event"
 )
 
 //export wok
 func wok(e event.Event) uint32 {
-  h, err := e.HTTP()
-  if err != nil {
-   return 1
-  }
+  	h, err := e.HTTP()
+		if err != nil {
+		return 1
+	}
 
-  h.Write([]byte("PONG"))
+	db, err := database.New("testdb")
+	if err != nil {
+		return 1
+	}
+
+	data, err := db.Get("value/hello")
+	if err != nil {
+		return 1
+	}
+
+	j,err := json.MarshalIndent(data,""," ")
+	if err != nil{
+		return 1
+	}
+
+ 	h.Write([]byte(j))
   
-  return 0
+  	return 0
 }
