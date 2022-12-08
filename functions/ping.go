89cdfa1d55a918b693775b91d7fc86edb7fda771
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-07 20:12:03 -0600

    reading data back from db now

--- functions/ping.go
+++ functions/ping.go
@@ -1,43 +1,43 @@
 package lib
 
 import (
+	"strings"
+
 	"bitbucket.org/taubyte/go-sdk/database"
 	"bitbucket.org/taubyte/go-sdk/event"
 )
-var (
-	// testId   = uint32(5)
-	// testData = map[string][]byte{}
-)
+
 //export ping
 func ping(e event.Event) uint32 {
-	// h, err := e.HTTP()
+	db, err := database.New("testdb")
+	
+	if err != nil {
+		return 1
+	}
+
+	// err = db.Put("value/hello", []byte("Hello, world"))
 	// if err != nil {
 	// 	return 1
 	// }
-
-	// h.Write([]byte("PONG"))
-
-	// return 0
-	db, err := database.New("testdb")
-	if err != nil {
-		return 1
-	}
-
-	err = db.Put("value/hello", []byte("Hello, world"))
-	if err != nil {
-		return 1
-	}
 
 	// err = db.Put("value/hello2", []byte("Hello, world"))
 	// if err != nil {
 	// 	return 1
 	// }
 
-	// keys, err := db.List("value")
-	// if len(keys) != 2 || err != nil {
-	// 	return 1
-	// }
+	keys, err := db.List("value")
+	if len(keys) != 2 || err != nil {
+		return 1
+	}
 
+	//Get HTTP from the event
+	h, err := e.HTTP()
+	if err != nil { //If we get an err 
+		return 1 //roll out
+	}
+
+	//Otherwise write the keys to the page cuz why not?
+	h.Write([]byte(strings.Join(keys, ",")))
 	// data, err := db.Get("value/hello")
 	// if err != nil {
 	// 	return 1
@@ -52,11 +52,12 @@ func ping(e event.Event) uint32 {
 	// 	return 1
 	// }
 
-	// data, err = db.Get("value/hello")
-	// if err == nil {
-	// 	return 1
-	// }
 
+	data, err := db.Get("value/hello")
+	if err == nil {
+		return 1
+	}
+	h.Write([]byte(data))
 	// err = db.Close()
 	// if err != nil {
 	// 	return 1
