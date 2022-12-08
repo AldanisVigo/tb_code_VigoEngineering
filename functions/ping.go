bfd55713d4f6eae166a08530da9d61ee7f34c4d8
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-07 21:15:59 -0600

    reading from db

--- functions/ping.go
+++ functions/ping.go
@@ -7,25 +7,17 @@ import (
 
 //export ping
 func ping(e event.Event) uint32 {
-	data, err := database.Database(5).Get("value/hello")
+	//Get the database reference
+	db, err := database.New("testdb")
 	if err != nil {
 		return 1
 	}
 
-	// err = db.Put("value/hello", []byte("Hello, world"))
-	// if err != nil {
-	// 	return 1
-	// }
-
-	// err = db.Put("value/hello2", []byte("Hello, world"))
-	// if err != nil {
-	// 	return 1
-	// }
-
-	// keys, err := db.List("value")
-	// if len(keys) != 2 || err != nil {
-	// 	return 1
-	// }
+	//Get the data from the database
+	data,err := db.Get("value/hello")
+	if err != nil {
+		return 1
+	}
 
 	//Get HTTP from the event
 	h, err := e.HTTP()
@@ -33,33 +25,7 @@ func ping(e event.Event) uint32 {
 		return 1 //roll out
 	}
 
-	//Otherwise write the keys to the page cuz why not?
-	// h.Write([]byte(strings.Join(keys, ",")))
-	// data, err := db.Get("value/hello")
-	// if err != nil {
-	// 	return 1
-	// }
-
+	//Send the data back to the browser
 	h.Write(data)
-	// if string(data) != "Hello, world" {
-	// 	return 1
-	// }
-
-	// err = db.Delete("value/hello")
-	// if err != nil {
-	// 	return 1
-	// }
-
-
-	// data, err := db.Get("value/hello")
-	// if err == nil {
-	// 	return 1
-	// }
-	// h.Write([]byte(data))
-	// err = db.Close()
-	// if err != nil {
-	// 	return 1
-	// }
-
 	return 0
 }
