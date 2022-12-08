bec0c6d233723b5fc9e465f75357d86f718b9758
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-08 06:13:17 -0600

    updated funcs

--- functions/rd.go
+++ functions/rd.go
@@ -2,28 +2,27 @@ package lib
 
 //Import the necessary libraries
 import (
-	"bitbucket.org/taubyte/go-sdk/database"
 	"bitbucket.org/taubyte/go-sdk/event"
 )
 
 //export
 func rd(e event.Event) uint32 {
 	//Get the database reference
-	db, err := database.New("testdb")
-	if err != nil {
-		return 1
-	}
+	// db, err := database.New("testdb")
+	// if err != nil {
+	// 	return 1
+	// }
 
-	err = db.Put("value/hello", []byte("Hello, world"))
-	if err != nil {
-		return 1
-	}
+	// err = db.Put("value/hello", []byte("Hello, world"))
+	// if err != nil {
+	// 	return 1
+	// }
 
-	//Get the data from the database
-	data,err := db.Get("value/hello")
-	if err != nil {
-		return 1
-	}
+	// //Get the data from the database
+	// data,err := db.Get("value/hello")
+	// if err != nil {
+	// 	return 1
+	// }
 
 	//Get HTTP from the event
 	h, err := e.HTTP()
@@ -38,7 +37,7 @@ func rd(e event.Event) uint32 {
 	// }
 
 	//Send the data back to the browser
-	h.Write(data)
+	h.Write([]byte("Hello I Work Again"))
 
 	//Return 0 cuz IDK, just do it
 	return 0
