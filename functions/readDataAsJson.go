290425fcc76ac43cde41dff8c3f7f3d92ea37da7
AldanisVigo <aldanisvigo@gmail.com>
2022-12-08 05:57:33 -0600

    trying stuff till it wirk

--- functions/readDataAsJson.go
+++ functions/readDataAsJson.go
@@ -7,29 +7,35 @@ import (
 )
 
 //export
-func rd(e event.Event) uint32 {
+func readDataAsJson(e event.Event) uint32 {
 	//Get the database reference
 	db, err := database.New("testdb")
 	if err != nil {
+		println(err)
 		return 1
 	}
+	println(db)
 
-	err = db.Put("value/hello", []byte("Hello, world"))
-	if err != nil {
-		return 1
-	}
+	// err = db.Put("value/hello", []byte("Hello, world"))
+	// if err != nil {
+	// 	println(err)
+	// 	return 1
+	// }
 
 	//Get the data from the database
 	data,err := db.Get("value/hello")
 	if err != nil {
+		println(err)
 		return 1
 	}
+	println(data)
 
 	//Get HTTP from the event
 	h, err := e.HTTP()
 	if err != nil { //If we get an err 
 		return 1 //roll out
 	}
+	println(h)
 
 	//Generate a json response	
 	// jsonData, err := json.MarshalIndent(data, "", "  ")
@@ -38,7 +44,13 @@ func rd(e event.Event) uint32 {
 	// }
 
 	//Send the data back to the browser
-	h.Write(data)
+	w,err := h.Write(data)
+	if err != nil {
+		println(err)
+		return 1
+	}
+
+	println(w)
 
 	//Return 0 cuz IDK, just do it
 	return 0
