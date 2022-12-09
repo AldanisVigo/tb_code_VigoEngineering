3c367564c04afb46414c3885f8cef87ddb608c96
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-08 19:31:22 -0600

    body json

--- functions/adduser.go
+++ functions/adduser.go
@@ -2,7 +2,7 @@ package lib
 
 import (
 	"io/ioutil"
-	"bitbucket.org/taubyte/go-sdk/database"
+
 	"bitbucket.org/taubyte/go-sdk/event"
 )
 
@@ -14,11 +14,11 @@ func adduser(e event.Event) uint32 {
 		return 1
 	}
 
-	//Get a reference to the database
-	db, err := database.New("testdb")
-	if err != nil {
-		return 1
-	}
+	// //Get a reference to the database
+	// db, err := database.New("testdb")
+	// if err != nil {
+	// 	return 1
+	// }
 
 	//Get the Body in the HTTP object
 	body := h.Body()
@@ -33,13 +33,11 @@ func adduser(e event.Event) uint32 {
 		return 1
 	}
 
-
-
-	//Close the db
-	err = db.Close()
-	if err != nil {
-		return 1
-	}
+	// //Close the db
+	// err = db.Close()
+	// if err != nil {
+	// 	return 1
+	// }
 
 	//Return what we get
  	h.Write([]byte(bodyData))
