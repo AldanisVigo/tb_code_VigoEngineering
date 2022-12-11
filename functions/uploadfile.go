045a0948efc6bf3ab2336552e285777cbafac376
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-11 00:54:16 -0600

    missing } in json response, fixed

--- functions/uploadfile.go
+++ functions/uploadfile.go
@@ -64,7 +64,7 @@ func uploadfile(e event.Event) uint32 {
 		return 1 //Eject
 	}
 
-	w,err := h.Write([]byte("{ \"file\" : \"" + string(incomingFileUploadRequest.file) + "\", \"path\" : \"" + incomingFileUploadRequest.UUID + "/" + incomingFileUploadRequest.name + "\""))
+	w,err := h.Write([]byte("{ \"file\" : \"" + string(incomingFileUploadRequest.file) + "\", \"path\" : \"" + incomingFileUploadRequest.UUID + "/" + incomingFileUploadRequest.name + "\"}"))
 	fmt.Print(w)
 
 	// //Save the file in the json request to the file storage at the uuid/name/file path
