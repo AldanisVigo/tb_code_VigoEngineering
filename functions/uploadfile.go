8d8c993e323668a4de3a7538ed338e58fbf7dbda
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-10 23:55:12 -0600

    printing fileUploadContents and returning them to client

--- functions/uploadfile.go
+++ functions/uploadfile.go
@@ -65,28 +65,31 @@ func uploadfile(e event.Event) uint32 {
 		return 1 //Eject
 	}
 
-	//Save the file in the json request to the file storage at the uuid/name/file path
-	file := testStorage.File(incomingFileUploadRequest.UUID + "/" + incomingFileUploadRequest.name)
-	
-	//Add the file and get the version of the file
-	version , err := file.Add(incomingFileUploadRequest.file, true)
-	if err != nil { //If there's an error while adding the file to the storage
-		//Write a response to the client letting them know about the error
-		h.Write([]byte("{ \"error\" : \"" + err.Error() + "\", \"msg\" : \"There was an error addin the file to the test storage in the dFunction.\"}"))
-		return 1 //Eject
-	}
-
-	//Print the version of the file
-	fmt.Print(version)
-	
-	//Return a response to the caller
-	w, err := h.Write([]byte("{ \"UUID\" : \"" + incomingFileUploadRequest.UUID + "\" , \"filename\" : \"" + incomingFileUploadRequest.name + "\",\"file_uploaded\" : true }"))
-	if err != nil { //If there's an error while writing a response back 
-		fmt.Print(err) //Print the error
-	}
-
-	//Print the result of writing a response
+	w,err := h.Write(fileUploadRequestContents)
 	fmt.Print(w)
+
+	// //Save the file in the json request to the file storage at the uuid/name/file path
+	// file := testStorage.File(incomingFileUploadRequest.UUID + "/" + incomingFileUploadRequest.name)
+	
+	// //Add the file and get the version of the file
+	// version , err := file.Add(incomingFileUploadRequest.file, true)
+	// if err != nil { //If there's an error while adding the file to the storage
+	// 	//Write a response to the client letting them know about the error
+	// 	h.Write([]byte("{ \"error\" : \"" + err.Error() + "\", \"msg\" : \"There was an error adding the file to the test storage in the dFunction.\"}"))
+	// 	return 1 //Eject
+	// }
+
+	// //Print the version of the file
+	// fmt.Print(version)
+	
+	// //Return a response to the caller
+	// w, err := h.Write([]byte("{ \"UUID\" : \"" + incomingFileUploadRequest.UUID + "\" , \"filename\" : \"" + incomingFileUploadRequest.name + "\",\"file_uploaded\" : true }"))
+	// if err != nil { //If there's an error while writing a response back 
+	// 	fmt.Print(err) //Print the error
+	// }
+
+	// //Print the result of writing a response
+	// fmt.Print(w)
 
 	//Successful operation
   	return 0
