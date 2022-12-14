ee9f1ec5bfd205c0f5d58f9b7a1bc1f040e6a6c1
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-14 05:05:47 -0600

    added the taulistEndpoint dFunc

---
+++ functions/taulistEndpoint.go
@@ -0,0 +1,42 @@
+package lib
+
+import (
+	"fmt"
+
+	"bitbucket.org/taubyte/go-sdk/event"
+)
+
+//export taulistEndpoint
+func taulistEndpoint(e event.Event) uint32 {
+	//Get the HTTP request
+	h,err := e.HTTP()	
+	if err != nil { //If we have an error getting the HTTP request
+		h.Write([]byte(fmt.Sprintf("ERROR: %s\n",err))) //Let the user know that we had an error
+	}
+
+	//Set the response header's content type to application/json
+	err = h.Headers().Set("Content-Type","application/json")
+
+	//Once we have the HTTP request, retrieve and return the path to the user
+	err = retrieveRequestPath(h)
+	if err != nil { //If there's an error while retrieving the path
+		h.Write([]byte(fmt.Sprintf("ERROR: %s\n",err))) //Send an error back to the client
+	}
+
+	//Successful execution
+	return 0;
+}
+
+func retrieveRequestPath(h event.HttpEvent) error {
+	//Get the path from the http event
+	path,err := h.Path()
+	if err != nil { //If we have an issue getting the path from the HTTP request
+		return err //Return the error
+	}
+
+	//Write the path back to the client
+	h.Write([]byte(path))
+	
+	//Successful execution, return nil for error
+	return nil
+}
