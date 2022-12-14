c699c8732e4e70bc625a762e9b2e5efce721871d
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-14 04:52:52 -0600

    last push

--- functions/getuserbyid.go
+++ functions/getuserbyid.go
@@ -1,7 +1,9 @@
 package lib
 
 import (
+	"fmt"
 	"io/ioutil"
+
 	"bitbucket.org/taubyte/go-sdk/database"
 	"bitbucket.org/taubyte/go-sdk/event"
 )
@@ -19,49 +21,67 @@ type UserRequest struct {
 func getuserbyid(e event.Event) uint32 {
 	//Get the http object from the event
   	h, err := e.HTTP()
-		if err != nil {
+	if err != nil {
 		return 1
 	}
 
-	// //Get a reference to the database
-	db, err := database.New("testdb")
+	//Attempt to retrieve the user's data from the database
+	err = retrieveUserFromDatabase(h)
 	if err != nil {
-		return 1
+		h.Write([]byte(fmt.Sprintf("Error encountered: %s",err)))
+	}
+
+	//Execution successful
+	return 0
+}
+
+func retrieveUserFromDatabase(h event.HttpEvent) error {
+	//Get the test database
+	db, err := database.New("testdb")
+	if err != nil { //If we encounter an error getting the database
+		return err //Return the error
 	}
 
 	//Get the Body in the HTTP object
 	body := h.Body()
-	bodyData, err := ioutil.ReadAll(body)
-	if err != nil {
-		return 1
+	bodyData, err := ioutil.ReadAll(body) //Read the contents of the request body
+	if err != nil { //If we encounter an error reading the contents of the request body
+		return err //Return the error
 	}
 
 	//Close the body
-	err = body.Close()
-	if err != nil {
-		return 1
+	err = body.Close() 
+	if err != nil { //If we encounter an error closing the request body
+		return err //Return the error
 	}
 
 	//Create an empty user
-	incomingUser := &UserRequest{}
+	incomingUserRequest := &UserRequest{}
 
 	//Fill it with the unmarshalled json version of the body data
-	incomingUser.UnmarshalJSON(bodyData)
+	incomingUserRequest.UnmarshalJSON(bodyData)
 
-	//Save the user JSON to the the database
-	data, err := db.Get(incomingUser.UUID)
-	if err != nil {
-		return 1
+	//Get the user JSON from the the database
+	data, err := db.Get(incomingUserRequest.UUID)
+	if err != nil { //If we encounter an error getting the current user
+		return err //Return an error
 	}
 	
 	//Close the db
 	err = db.Close()
-	if err != nil {
-		return 1
+	if err != nil { //If we encounter an error while closing the database
+		return err //Return the error
 	}
 	
 	//Return a response to the caller
-	h.Write([]byte(data))
+	w,err := h.Write([]byte(data))
+	if err != nil {
+		return err
+	}
 
-  	return 0
+	//Print the results of the write
+	fmt.Print(w)
+
+	//Execution successful, return nil for error
+  	return nil
 }
